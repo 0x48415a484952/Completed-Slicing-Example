c78bfdfe475130946f87a2f7142afbc0e2eb4b24
Hazhir Ahmadzadeh <hazhir1811@gmail.com>
2020-01-27 22:07:54 +0330

    removed ArrayReverse and merged it with splitnmbr

--- hazhir.c
+++ hazhir.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
 
-// checkUserNationalCodeAgainstInvalidCodes(userInput) {
-//     for(int i = 0; i <= size)
-// }
-
 int count(long number) {
     int count = 0;
     while(number != 0) {
@@ -20,7 +16,6 @@ int count(long number) {
 //     return count;
 // }
 
-//we have a warning here
 int checkUserInputAgainstInvalidCodes(long userInput) {
     long invalidCodes[] = {
         1111111111,
@@ -41,50 +36,25 @@ int checkUserInputAgainstInvalidCodes(long userInput) {
     return 0;
 }
 
-int * splitNumber(long number) {
+int * explodeAndReverse(long number) {
     int array[9];
-    for(int i = 9; i >= 0; i--) {
+    for(int i = 0; i <= 9; i++) {
         array[i] = number % 10;
         number /= 10;
+        printf("%d", array[i]);
     }
+    printf("\n");
     int *ptr = &array[0];
-    //printing splited number
-    // for(int i = 0; i < 10; i++) {
-    //     printf("%d \n", array[i]);
-    // }
+
     return ptr;
 }
-
-int * reverseArray(int *ptr) {
-    int reversedArray[9];
-    int j = sizeof(int);
-    for(int i = 0; i <= 9; i++) {
-        reversedArray[9 - i] = *ptr;
-        ptr += j;
-    }
-    //printing reversedArray
-    for(int i = 0; i < 10; i++) {
-        printf("%d \n", reversedArray[i]);
-    }
-    // int *reverseAraayPtr = &reversedArray[0];
-    // return reverseAraayPtr;
-}
-
-// int explodeAndReverse(long number) {
-//     long array = 
-// }
 
 int main() {
     long nationalCode;
     printf("Enter a National Code: ");
     scanf("%ld", &nationalCode);
     printf("Your National Code Is: %ld \n and the size of it is: %d \n", nationalCode, count(nationalCode));
-    // printf("the splited version of your national code is: %d \n", splitNumber(nationalCode));
-
-    // splitNumber(nationalCode);
-    //when passing splitnumber to reversearray we get a segmentation fault
-    reverseArray(splitNumber(nationalCode));
-    return 0;
+    explodeAndReverse(nationalCode);
     // reverseArray(splitNumber(nationalCode));
     if(checkUserInputAgainstInvalidCodes(nationalCode)) {
         printf("not valid \n");
